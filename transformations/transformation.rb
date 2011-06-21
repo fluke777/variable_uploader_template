0653097d955e7bfb765cfc07b67eea40f3802b72
Tomas Svarovsky <svarovsky@gooddata.com>
2011-06-21 03:38:40 +0200

    Changed symbols to strings

--- transformations/transformation.rb
+++ transformations/transformation.rb
@@ -3,9 +3,9 @@ require 'bundler/setup'
 require 'user_hierarchies'
 
 GoodData::UserHierarchies::UserHierarchy::read_from_csv("../data/data.csv", {
-    :user_id => :Id,
-    :manager_id => :ManagerId,
-    :description_field => :Email
+    :user_id => "Id",
+    :manager_id => "ManagerId",
+    :description_field => "Email"
 }) do |hierarchy|
     hierarchy.users.each do |user|
         puts "#{user.Email} => #{user.all_subordinates.map {|s| s.Email}.join(", ")}"
