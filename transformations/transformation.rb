e416a8904b227ece485e6006a99940847d13144e
Tomas Svarovsky <svarovsky@gooddata.com>
2011-06-21 03:44:24 +0200

    Adde the __FILE__ so the transformation is not dependent on pwd

--- transformations/transformation.rb
+++ transformations/transformation.rb
@@ -2,10 +2,10 @@ require 'rubygems'
 require 'bundler/setup'
 require 'user_hierarchies'
 
-GoodData::UserHierarchies::UserHierarchy::read_from_csv("../data/data.csv", {
-    :user_id => "Id",
-    :manager_id => "ManagerId",
-    :description_field => "Email"
+GoodData::UserHierarchies::UserHierarchy::read_from_csv(File.expand_path("../../data/data.csv", __FILE__), {  
+  :user_id => "Id",
+  :manager_id => "ManagerId",
+  :description_field => "Email"
 }) do |hierarchy|
     hierarchy.users.each do |user|
         puts "#{user.Email} => #{user.all_subordinates.map {|s| s.Email}.join(", ")}"
