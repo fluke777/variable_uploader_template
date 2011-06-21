f0d0fd5fcb48b0b9f06e942ce57cc674d3c95adb
Tomas Svarovsky <svarovsky@gooddata.com>
2011-06-21 03:54:53 +0200

    Added some more examples

--- transformations/transformation.rb
+++ transformations/transformation.rb
@@ -8,6 +8,8 @@ GoodData::UserHierarchies::UserHierarchy::read_from_csv(File.expand_path("../../
   :description_field => "Email"
 }) do |hierarchy|
     hierarchy.users.each do |user|
-        puts "#{user.Email} => #{user.all_subordinates.map {|s| s.Email}.join(", ")}"
+      puts "#{user.Email} => #{([user] + user.all_subordinates).map {|s| s.Email}.join(", ")}"
+      # puts "#{user.Email} => #{user.all_subordinates.size}"
+      # puts "#{user.Email}" if user.is_manager?
     end
 end
