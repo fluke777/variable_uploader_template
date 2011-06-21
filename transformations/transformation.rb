111fabacbde4b71b9646e91f9eba6b1c25aad0fd
Tomas Svarovsky <svarovsky@gooddata.com>
2011-06-21 03:05:18 +0200

    Added data and transformation

---
+++ transformations/transformation.rb
@@ -0,0 +1,13 @@
+require 'rubygems'
+require 'bundler/setup'
+require 'user_hierarchies'
+
+GDC::UserHierarchies::read_from_csv("../data/data.csv", {
+    :user_id => :Id,
+    :manager_id => :ManagerId,
+    :description_field => :Email
+}) do |hierarchy|
+    hierarchy.users.each do |user|
+        puts "#{user.Email} => #{user.all_subordinates.map {|s| s.Email}.join(", ")}"
+    end
+end
